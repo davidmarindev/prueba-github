8cf47e68afc7034e7d31c87f28bfe462a99dcd49
David Marin <davidmaring1997@gmail.com>
2026-03-04 21:29:27 -0400

    Cambios en main

---
+++ main.py
@@ -0,0 +1,19 @@
+def suma(a, b):
+    return a + b
+
+print(suma(1, 2))
+
+def resta(a, b):
+    return a - b
+
+print(resta(1, 2))
+
+def multiplicacion(a, b):
+    return a * b
+
+print(multiplicacion(1, 2))
+
+def division(a, b):
+    return a / b
+
+print(division(1, 2))
